953c6f5cdc9e97300ac41bd327f35ce5d30fc9e7
fuyanwei <vipfuyanwei@163.com>
2018-01-30 17:45:49 +0800

    tmall_ssm

---
+++ src/main/java/com/yanglies/tmall/test/Test.java
@@ -0,0 +1,15 @@
+package com.yanglies.tmall.test;
+
+/**
+ * lies, please leave something
+ *
+ * @author lies
+ * @Createdon 2018/1/30 10:15.
+ * @ProjectName tmall_ssm
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(1<=1);
+    }
+}
